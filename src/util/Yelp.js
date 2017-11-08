5f5b0534c14c05419dec7442681122dcb7beb362
Viet Ngan Ly Phat <lyphat.vietngan@gmail.com>
2017-11-08 18:25:02 +0100

    yelp debug 2

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -16,7 +16,7 @@ let Yelp = {
   search(term, location, sortBy) {
     return Yelp.getAccessToken().then(() => {return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {headers: {Authorization: `Bearer ${accessToken}`}});}).then(jsonResponse => {
       if(jsonResponse.businesses) {
-        return jsonResponse.businesses.map(business => ({
+        return jsonResponse.businesses.map(business => {return {
           id: business.id,
           imageSrc: business.image_url,
           name: business.name,
@@ -27,8 +27,7 @@ let Yelp = {
           category: business.categories,
           rating: business.rating,
           reviewCount: business.review_count
-        }));
-      }
+        };})
       console.log(jsonResponse);
     });
   }
